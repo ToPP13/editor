b9a88a454642aceafd3ad62fdda1b02727394846
Kirill <Kirill>
2020-11-03 09:52:12 +0300

    Delete all old function calls in main

--- src/editor.cpp
+++ src/editor.cpp
@@ -13,21 +13,5 @@
 
 int main(int, char *[]) {
 
-    char test_char(-1);
-    print_ip(test_char);
-    std::cout << std::endl;
-
-    short test_short(0);
-    print_ip(test_short);
-    std::cout << std::endl;
-
-    int test_int(2130706433);
-    print_ip(test_int);
-    std::cout << std::endl;
-
-    long test_long(8875824491850138409);
-    print_ip(test_long);
-    std::cout << std::endl;
-
     return 0;
 }
